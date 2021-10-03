ebf9762d1b2f2f96355d0222b9a07ffcb0cd4dde
alextk1978 <alextk@bk.ru>
2021-10-03 14:05:51 +0300

    Добавил автосоздание таблиц

--- src/main/java/web/config/AppConfig.java
+++ src/main/java/web/config/AppConfig.java
@@ -54,6 +54,7 @@ public class AppConfig {
     private Properties additionalProperties() {
         Properties properties = new Properties();
         properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+        properties.setProperty("hibernate.hbm2ddl.auto", "update");
 
         return properties;
     }
